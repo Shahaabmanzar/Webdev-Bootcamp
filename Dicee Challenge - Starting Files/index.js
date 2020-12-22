374df4f4d5389ea46dceeaf551c4039ffb16c1e9
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-22 18:50:19 +0530

    save

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -2,13 +2,12 @@ var randomNumber1=Math.floor((Math.random() * 6) + 1);
 document.firstElementChild.lastElementChild.firstElementChild.querySelector("DIV IMG").setAttribute("src","/images/dice"+randomNumber1+".png");
 var randomNumber2=Math.floor((Math.random() * 6) + 1);
 document.firstElementChild.lastElementChild.firstElementChild.lastElementChild.lastElementChild.setAttribute("src","/images/dice"+randomNumber2+".png");
-{if(randomNumber1>randomNumber2){
-    document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML="Player1 wins";
+if(randomNumber1>randomNumber2){
+    document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML="Player 1 wins";
 }
-else  if(randomNumber1<randomNumber2) {
-    document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML="player2 wins";
+else  if(randomNumber1< randomNumber2) {
+    document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML="Player 2 wins";
 }else{
     document.firstElementChild.lastElementChild.firstElementChild.firstElementChild.innerHTML="Draw";
 }
 
-}
