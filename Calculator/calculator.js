13dfe41e88de5e1e2c4b2cabe76e7535399546d3
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-26 02:07:49 +0530

    save

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -20,10 +20,10 @@ app.get("/bmiCalculator", function(req, res){
     res.sendFile(__dirname+"/bmiCalculator.html");
 });
 app.post("/bmiCalculator", function(req,res){
-    var weight=parseFloat(req.body.weight);
-    var height=parseFloat(req.body.height);
-    var bmi=weight/(height*height);
-    res.send=("Your BMI is: "+bmi);
+    var weight=parseFloat(req.body.Weight);
+    var height=parseFloat(req.body.Height);
+    var bmi=Math.round(weight/(height*height));
+    res.send("Your BMI is: "+bmi);
 });
 app.listen(3000,function(){
     console.log("server started on port 3000");
