aae312cd20434167d7d6b20c970e39b54ad498bd
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-28 15:46:22 +0530

    save

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,19 +1,36 @@
 const express=require("express");
 const https=require("https");
+const bodyParser=require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
-    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=6014252a132a06877188f62dedd05b74&units=metric";
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req, res){
+    const query=req.body.cityName;
+    const apiKey="6014252a132a06877188f62dedd05b74"
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=metric";
+    
     https.get(url,function(response){
         console.log(response.statusCode);
 
         response.on("data",function(data){
         const weatherData=JSON.parse(data);
         const temp=weatherData.main.temp
-        const weatherDescription=weatherData.weather[0].weatherDescription
-        res.send("<h1>The temperature in london is "+temp+" degree</h1>");
+        const weatherDescription=weatherData.weather[0].description
+        const icon=weatherData.weather[0].icon
+        const imageURL ="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+        res.set("content-type", "text/html"); //not in the vedio andreas taught us else it was returingin the h1 as awell
+        res.write("The Weather is currently "+ weatherDescription);
+        res.write("<h1>The temperature in "+query+" is "+temp+" degree</h1>");
+        res.write("<img src="+imageURL+">")
+        res.send();
         })
     })
 })
+
 
 
 
