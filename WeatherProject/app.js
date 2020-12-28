6273c4a1b6b69ff8f6665ec3771371868f8aea53
Shahaab Manzar <mshahaabmanzar@gmail.com>
2020-12-28 13:39:32 +0530

    save

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,7 +1,18 @@
 const express=require("express");
+const https=require("https");
 const app = express();
 app.get("/",function(req,res){
-    res.send("server is up and running.")
+    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=6014252a132a06877188f62dedd05b74&units=metric";
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+        const weatherData=JSON.parse(data);
+        const temp=weatherData.main.temp
+        const weatherDescription=weatherData.weather[0].weatherDescription
+        res.send("<h1>The temperature in london is "+temp+" degree</h1>");
+        })
+    })
 })
 
 
