64e769efd9581840e54057c2c129716c70efb84b
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-01 11:41:50 +0530

    save

--- todolist-v2 (mongoose)/app.js
+++ todolist-v2 (mongoose)/app.js
@@ -1,8 +1,7 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose=require("mongoose");
 
 const app = express();
 
@@ -11,12 +10,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
+
+const itemsSchema={
+  name: String
+};
+
+const Item=mongoose.model("Item",itemsSchema);
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+// const day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
 
