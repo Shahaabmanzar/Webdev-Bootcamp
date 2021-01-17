b6c6f0d349fb883210e7ca4dd984e237055b0d42
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-17 20:26:35 +0530

    Sidebar added

--- portfolio-v3/react-portfilio-v3/src/components/Sidebar/index.js
+++ portfolio-v3/react-portfilio-v3/src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SidebarContainer, Icon, CloseIcon } from './SidebarElements'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements';
 
 const Sidebar = () => {
     return (
@@ -7,6 +7,28 @@ const Sidebar = () => {
             <icon>
                 <CloseIcon />
             </icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About Me
+                </SidebarLink>
+                    <SidebarLink to="projects">
+                        Projects
+                </SidebarLink>
+                    <SidebarLink to="Blogs">
+                        Blogs
+                </SidebarLink>
+                    <SidebarLink to="letscollaborate">
+                        Lets Collaborate
+                </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    {/* router link so slash is must in the next line */}
+                    <SidebarRoute to="/signin">
+                        Sign In
+                    </SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
     )
 }
