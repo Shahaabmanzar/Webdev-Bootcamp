85c802ab4de3e8058bea0926f3499a5e3f0d16ba
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-18 00:55:41 +0530

    commit

--- portfolio-v3/react-portfilio-v3/src/components/HeroSection/index.js
+++ portfolio-v3/react-portfilio-v3/src/components/HeroSection/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg }
+import Video from '../../videos/video.mp4';
+import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
 
 const HeroSection = () => {
     return (
-        <HeroContainer id="home">
+        <HeroContainer>
             <HeroBg>
-                <VideoBg autoplay loop muted src={Video} type='video/mp4' />
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
 
+                <HeroContent>
+                    <heroH1>
+                        Shahaab Manzar
+                </heroH1>
+                    <HeroP>
+
+                    </HeroP>
+                </HeroContent>
             </HeroBg>
         </HeroContainer>
     )
--- portfolio-v3/react-portfilio-v3/src/pages/index.js
+++ portfolio-v3/react-portfilio-v3/src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
