334a24f43d5bb6d89b49e4e16d5e929aa1e6c885
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-20 01:28:31 +0530

    adding images

---
+++ portfolio-v3/react-portfilio-v3/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: `'I'm baby chambray'`,
+    headLine: `'I'm baby chambray blue bottle blog adaptogen'`,
+    description: `' Readymade try-hard organic, master cleanse whatever seitan salvia scenester fashion axe flannel bicycle rights selfies pitchfork taxidermy. Drinking vinegar biodiesel slow-carb vaporware selfies four dollar toast.'`,
+    buttonLabel: 'peek',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
