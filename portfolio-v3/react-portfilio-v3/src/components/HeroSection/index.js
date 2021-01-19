17ebfd0037334b00ec8bc1f03a7f3a8d3eb85cf5
Shahaab Manzar <mshahaabmanzar@gmail.com>
2021-01-19 23:19:41 +0530

    adding button and front page

--- portfolio-v3/react-portfilio-v3/src/components/HeroSection/index.js
+++ portfolio-v3/react-portfilio-v3/src/components/HeroSection/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+import { Button } from '../ButtonElement';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
-
-                <HeroContent>
-                    <heroH1>
-                        Shahaab Manzar
-                </heroH1>
-                    <HeroP>
-
-                    </HeroP>
-                </HeroContent>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Shahaab Manzar
+                </HeroH1>
+                <HeroP>
+                    Front-end Developer.
+                    </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
+                        primary="true"
+                        dark="true"
+                    >
+                        Lets Collaborate {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
-    )
-}
+
+    );
+};
 
 export default HeroSection;
